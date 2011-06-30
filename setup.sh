2a610afe7e04d9a8705e9a65312bf01a159659e7
Ethan Brown <ethan.w.brown@gmail.com>
2011-06-30 15:46:22 -0400

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,12 @@
 
 git submodule init && git submodule update
 
-if [ -e ~/.vimrc ]
-then
+if [ -e ~/.vimrc ]; then
   echo "~/.vimrc exists."
   echo "Do you want backup existing ~/.vimrc? (Y/N)"
   read a
   if [[ $a == "Y" || $a == "y" ]]; then
-    echo "Moving ~/.vimrc to ~/.vimrc_backup."
+    echo "Copying ~/.vimrc to ~/.vimrc_backup."
     cp ~/.vimrc ~/.vimrc_backup
   else
     echo "Overwritting existing ~/.vimrc"
