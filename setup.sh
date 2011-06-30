ef76a0f18619bea55c11610a15dc18808806a339
Ethan Brown <ethan.w.brown@gmail.com>
2011-06-30 17:02:45 -0400

    Moved directory to ~/vimfiles.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+cd ~/vimfiles
 git submodule init && git submodule update
+
+if [ -e ~/.vim ]; then
+  echo "~/.vim exists."
+  read -r -p "Do you want backup existing ~/.vim? (Y/N) " response
+  case $response in
+    [yY][eE][sS]|[yY]) 
+      echo "Moving ~/.vim to ~/.vim_backup."
+      mv -f ~/.vim ~/.vim_backup
+      ;;
+    *)
+      echo "Overwriting existing ~/.vim"
+      ;;
+  esac
+fi
+
+echo "Creating link from ~/vimfiles to ~/.vim"
+ln -sf ~/vimfiles ~/.vim
 
 if [ -e ~/.vimrc ]; then
   echo "~/.vimrc exists."
@@ -18,5 +36,6 @@ fi
 
 echo "Creating link from ~/.vimrc to ~/.vim/vimrc"
 ln -sf ~/.vim/vimrc ~/.vimrc
+
 
 echo "done."
