3b2c3f077df773db534585326fdde1fcb158bcaa
Ethan Brown <ethan.w.brown@gmail.com>
2011-06-30 15:40:44 -0400

    Modified setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 git submodule init && git submodule update
-ln -s ~/.vim/vimrc ~/.vimrc
+
+if [ -e ~/.vimrc ]
+then
+  echo "~/.vimrc exists."
+  echo "Do you want backup existing ~/.vimrc? (Y/N)"
+  read a
+  if [[ $a == "Y" || $a == "y" ]]; then
+    echo "Moving ~/.vimrc to ~/.vimrc_backup."
+    cp ~/.vimrc ~/.vimrc_backup
+  else
+    echo "Overwritting existing ~/.vimrc"
+  fi
+fi
+
+ln -sF ~/.vim/vimrc ~/.vimrc
 
 echo "done."
