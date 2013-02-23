3bc61c3583a0416142e0b07d8beeaefae94070dd
Ethan W Brown <ethan.w.brown@gmail.com>
2013-02-23 01:56:30 -0800

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-cd ${HOME}/vimfiles
-git submodule init && git submodule update
+if [ -e ${HOME}/.vimrc ]; then
+  echo "${HOME}/.vimrc exists."
+  read -r -p "Do you want backup existing ${HOME}/.vimrc? (Y/N) " response
+  case $response in
+    [yY][eE][sS]|[yY])
+      echo "Moving ${HOME}/.vimrc to ${HOME}/.vimrc_backup."
+      mv -f ${HOME}/.vimrc ${HOME}/.vimrc_backup
+      ;;
+    [nN]|[nN])
+      echo "Overwriting existing ${HOME}/.vimrc"
+      rm ${HOME}/.vimrc
+      ;;
+    *)
+      echo "ERROR: Unrecognized response!"
+      exit 1
+      ;;
+  esac
+fi
+
+echo "Creating link from ${HOME}/.vimrc to ${HOME}/.vim/vimrc"
+ln -sf ${HOME}/vimfiles/vimrc ${HOME}/.vimrc
 
 if [ -e ${HOME}/.vim ]; then
   echo "${HOME}/.vim exists."
   read -r -p "Do you want backup existing ${HOME}/.vim? (Y/N) " response
   case $response in
-    [yY][eE][sS]|[yY]) 
+    [yY][eE][sS]|[yY])
       echo "Moving ${HOME}/.vim to ${HOME}/.vim_backup."
       mv -f ${HOME}/.vim ${HOME}/.vim_backup
       ;;
-    *)
+    [nN]|[nN])
       echo "Overwriting existing ${HOME}/.vim"
+      rm -rf ${HOME}/.vim
+      ;;
+    *)
+      echo "ERROR: Unrecognized response!"
+      exit 1
       ;;
   esac
 fi
@@ -20,22 +44,8 @@ fi
 echo "Creating link from ${HOME}/vimfiles to ${HOME}/.vim"
 ln -sf ${HOME}/vimfiles ${HOME}/.vim
 
-if [ -e ${HOME}/.vimrc ]; then
-  echo "${HOME}/.vimrc exists."
-  read -r -p "Do you want backup existing ${HOME}/.vimrc? (Y/N) " response
-  case $response in
-    [yY][eE][sS]|[yY]) 
-      echo "Moving ${HOME}/.vimrc to ${HOME}/.vimrc_backup."
-      mv -f ${HOME}/.vimrc ${HOME}/.vimrc_backup
-      ;;
-    *)
-      echo "Overwriting existing ${HOME}/.vimrc"
-      ;;
-  esac
-fi
 
-echo "Creating link from ${HOME}/.vimrc to ${HOME}/.vim/vimrc"
-ln -sf ${HOME}/.vim/vimrc ${HOME}/.vimrc
-
+cd ${HOME}/vimfiles
+git submodule init && git submodule update
 
 echo "done."
